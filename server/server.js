561200f83d440f52e0caa1fd99c8ac78efa7592a
J2021T <80201567+J2021T@users.noreply.github.com>
2021-08-25 17:50:40 -0400

    added thought forms

--- server/server.js
+++ server/server.js
@@ -1,25 +1,31 @@
-const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 
-const {typeDefs, resolvers} = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({ 
-  typeDefs, 
-  resolvers, 
-  context: authMiddleware 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
 });
 
-server.applyMiddleware({ app });
+async function startApolloServer(typeDefs, resolvers) {
+  await server.start();
+  await server.applyMiddleware({
+    app
+  });
+}
+
+startApolloServer(typeDefs, resolvers);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
